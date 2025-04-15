c14cc26fe408d91d9fe7f8949454eaa34e75b8c0
MyoLatt <lattzinmyo3@gmail.com>
2025-04-15 08:42:38 +0630

    authentication

---
+++ app/Http/Middleware/JwtAuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+
+class JwtAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        try{
+            $user = JWTAuth::parseToken()->authenticate();
+        }catch(JWTException $e){
+            return response()->json([
+                'statusCode' => 401,
+                'message' => 'Token is invalid'
+            ], 401);
+        }
+        
+        return $next($request);
+    }
+}
